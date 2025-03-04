3e4faf790c20de6247fe520b23c6e969ea20c091
MMk <moulyaa2626@gmail.com>
2025-03-04 09:31:04 +0530

    ok

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,11 @@
-// write the book component code here
+import react from 'react'
+export const bookCard = ({books})=>{
+return(
+    <div>
+        <h1>{books.name}</h1>
+        <h2>{books.genre}</h2>
+        <h3>{books.id}</h3>
+        <h4>{books.author}</h4>
+    </div>
+)
+}
